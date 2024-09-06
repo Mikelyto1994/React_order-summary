50825a251e2ca3d8420336dac3e8fb4c72d5406a
Mikelyto1994 <mikelyto@gmail.com>
2024-09-06 16:11:48 -0500

    Configura la base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/React_order-summary/", // Reemplaza con el nombre de tu repositorio
+  build: {
+    outDir: "dist", // Directorio de salida para la construcción
+  },
+});
