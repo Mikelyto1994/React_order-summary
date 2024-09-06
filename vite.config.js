187d37955dfc3040f4eea2d953e4778df9b1f868
Miguel Pariona <157014665+Mikelyto1994@users.noreply.github.com>
2024-09-06 16:15:54 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React_order-summary/", // Reemplaza con el nombre de tu repositorio
-  build: {
-    outDir: "dist", // Directorio de salida para la construcción
-  },
+  plugins: [react()],
 });
+
